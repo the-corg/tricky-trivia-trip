55462aa18a4f79c6dc8b84dde58cafe3f5769ca2
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-05-09 00:05:36 +0200

    Handle API response codes and API exceptions

--- TrickyTriviaTrip/Api/TriviaApiClient.cs
+++ TrickyTriviaTrip/Api/TriviaApiClient.cs
@@ -28,33 +28,56 @@ namespace TrickyTriviaTrip.Api
 
         public async Task<IEnumerable<TriviaApiQuestion>> FetchNewQuestionsAsync(int amount = 10)
         {
-            await RequestTokenIfNullAsync();
-            // TODO: check that it's not null, fall back to DB
-            
-            // TODO: Delete this
-            Debug.WriteLine($"Current token: {_sessionToken}");
+            TriviaApiResponse? response = null;
 
-            var url = Settings.Default.TriviaApiBaseUrl + 
-                "?amount=" + amount +
-                "&type=multiple" +
-                "&token" + _sessionToken;
+            try
+            {
+                await RequestTokenIfNullAsync();
+                if (_sessionToken is null)
+                {
+                    Debug.WriteLine("Failed to get a session token from Trivia API");
+                    return Enumerable.Empty<TriviaApiQuestion>();
+                }
 
-            var response = await _httpClient.GetFromJsonAsync<TriviaApiResponse>(url, _serializerOptions);
+                Debug.WriteLine($"Current token: {_sessionToken}");
 
-            if (response is null)
-                // TODO: Change to normal validation without exception (falls back to DB)
-                throw new Exception("Failed to get or deserialize Trivia API response");
+                var url = Settings.Default.TriviaApiBaseUrl +
+                    "?amount=" + amount +
+                    "&type=multiple" +
+                    "&token" + _sessionToken;
 
-            // TODO: Handle the response codes
+                response = await _httpClient.GetFromJsonAsync<TriviaApiResponse>(url, _serializerOptions);
+
+                if (response is null)
+                {
+                    Debug.WriteLine("Failed to get or deserialize Trivia API response");
+                    return Enumerable.Empty<TriviaApiQuestion>();
+                }
+
+                if (response.ResponseCode != 0)
+                {
+                    await HandleResponseCode(response.ResponseCode);
+
+                    return Enumerable.Empty<TriviaApiQuestion>();
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine($"Trivia API error. Response code: {response?.ResponseCode}. Exception:\n"+ exception.ToString());
+                // Something went wrong with the API request, but that's not a big problem
+                return Enumerable.Empty<TriviaApiQuestion>();
+            }
 
             return response.Results;
         }
-        // TODO: catch exceptions, including JSON deserialization exceptions due to required properties
+
         #endregion
 
         #region Private helper methods 
 
-        // Requests a session token from Trivia API (if not requested earlier)
+        /// <summary>
+        /// Requests a session token from Trivia API (if not requested earlier)
+        /// </summary>
         private async Task RequestTokenIfNullAsync()
         {
             if (_sessionToken is not null)
@@ -64,10 +87,49 @@ namespace TrickyTriviaTrip.Api
                 Settings.Default.RetrieveTokenUrl, _serializerOptions);
 
             if (tokenResponse?.ResponseCode != 0 || string.IsNullOrEmpty(tokenResponse.Token))
-                // TODO: Change to normal validation without exception (falls back to DB)
-                throw new Exception("Failed to get a session token from Trivia API");
+                return;
 
             _sessionToken = tokenResponse.Token;
+        }
+
+        /// <summary>
+        /// Handles Trivia API response codes.
+        /// If necessary, refreshes the session token
+        /// </summary>
+        /// <param name="responseCode">Trivia API response code</param>
+        private async Task HandleResponseCode(int responseCode)
+        {
+            string reason;
+            switch (responseCode)
+            {
+                case 1:
+                    reason = "No Results.\nCould not return results. The API doesn't have enough questions for your query. (Ex. Asking for 50 Questions in a Category that only has 20.)";
+                    break;
+                case 2:
+                    reason = "Invalid Parameter.\nContains an invalid parameter. Arguments passed in aren't valid. (Ex. Amount = Five)";
+                    break;
+                case 3:
+                    reason = "Token Not Found.\nSession Token does not exist.";
+                    break;
+                case 4:
+                    reason = "Token Empty.\nSession Token has returned all possible questions for the specified query. Resetting the Token is necessary.";
+                    break;
+                case 5:
+                    reason = "Rate Limit.\nToo many requests have occurred. Each IP can only access the API once every 5 seconds.";
+                    break;
+                default:
+                    reason = "Undocumented API error";
+                    break;
+            }
+
+            Debug.WriteLine($"Unsuccessful API request. Code {responseCode}: " + reason);
+
+            if (responseCode == 3 || responseCode == 4)
+            {
+                // Refreshing the token
+                _sessionToken = null;
+                await RequestTokenIfNullAsync();
+            }
         }
         #endregion
     }
